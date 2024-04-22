d16a3f386b99da8939d8229fd959d45f0a77900e
Prateek Jadhav <134202260+Prateek0712@users.noreply.github.com>
2024-04-22 14:29:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+let sqrArr= document.querySelectorAll('.square');
+for(let s of sqrArr)
+	{
+		s.addEventListener('mouseover', changeColor);
+	}
+function  changeColor(event_details) {
+	for(let  s of sqrArr)
+		{
+			if(s.id!=event_details.target.id)
+			{
+				s.style.backgroundColor = '#6F4E37';
+			}
+			else
+			{
+				s.style.backgroundColor = '#E6E6FA';
+			}
+		}
+}
